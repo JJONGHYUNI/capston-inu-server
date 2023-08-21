5adbdb5c9c8998f3c0ab87f78072c3045e7ec5c3
JJONGHYUNI <whdgusdl8792@gmail.com>
2023-08-21 22:07:21 +0900

    [Feat] flask통신 dto 예시 수정
    Signed-off-by: JJONGHYUNI <whdgusdl8792@gmail.com>

--- capston/src/main/java/capston/server/photo/dto/CommunicationRequsetDto.java
+++ capston/src/main/java/capston/server/photo/dto/CommunicationRequsetDto.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Data
 @ApiModel(value = "플라스크 서버로 보낼 데이터")
 public class CommunicationRequsetDto {
-    @Schema(example = "https://capston.s3.ap-northeast-2.amazonaws.com/서울여행/1/2027cd25-0f4e-402a-a552-e26ff2344b71.jpeg")
+    @Schema(description = "예시 주소 : https://capston.s3.ap-northeast-2.amazonaws.com/서울여행/1/2027cd25-0f4e-402a-a552-e26ff2344b71.jpeg")
     private List<String> photoList;
     @Schema(example = "1")
     private int num;
